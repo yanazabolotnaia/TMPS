f3dd02ff0641853c8b0c23e21b659a4b2bc59f24
yanazabolotnaia <91680307+yanazabolotnaia@users.noreply.github.com>
2022-02-23 19:43:20 +0200

    Singleton and Abstract Factory

--- Program.cs
+++ Program.cs
@@ -1,142 +1,130 @@
-﻿using System;
+﻿using SinglAbstr;
+using System;
 
-namespace FactoryMethod
+namespace SinglAbstr
 {
+  
+    public interface AbstractFactory
+    {
+        public abstract IAbstractProductA CreateUserA();
+
+        private static AbstractFactory instance = null;
+        
+        //function to access the single instance object
+        public static AbstractFactory Instance()
+        {
+            if (instance == null)
+            {
+                instance = getInstance();
+            }
+            return instance;
+        }
+      
+        public static AbstractFactory getInstance()
+        {
+            Console.WriteLine("Введите цифру");
+            Console.WriteLine("1 - Admin\n2 - StakeHolder\n3 - ProjectManager ");
+            int userType = Int32.Parse(Console.ReadLine());
+            
+
+            switch (userType)
+            {
+                case 1:
+                    return new ConcreteFactory1();
+                case 2:
+                    return new ConcreteFactory2();
+                case 3:
+                    return new ConcreteFactory3();
+                default:return null;
+            }
+            
+        }
+        
+    }
+
+    class ConcreteFactory1 : AbstractFactory
+    {
+       
+
+        public IAbstractProductA CreateUserA()
+        {
+            return new ConcreteUserA1();
+        }
+
+
+    }
+
+    class ConcreteFactory2 : AbstractFactory
+    {
+        public IAbstractProductA CreateUserA()
+        {
+            return new ConcreteUserA2();
+        }
+
+    }
+    class ConcreteFactory3 : AbstractFactory
+    {
+        public  IAbstractProductA CreateUserA()
+        {
+            return new ConcreteUserA3();
+        }
+
+    }
+
+    public interface IAbstractProductA
+    {
+        string UsefulFunctionA();
+        
+    }
+
+
+    class ConcreteUserA1 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "Это страница админа";
+        }
+    }
+
+    class ConcreteUserA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "Это страница стэйк-холдера.";
+        }
+    }
+
+    class ConcreteUserA3 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "Это страница прожект-менеджера";
+        }
+    }
+
+
+    class User
+    {
+        public void Main()
+        {
+            AbstractFactory factory = AbstractFactory.Instance();
+            IAbstractProductA dc = factory.CreateUserA();
+            Console.WriteLine(dc.UsefulFunctionA());
+            
+
+        }
+
+        
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Client client1 = new ClientBuilder()
-                .Name("Alexei")
-                .Surname("Pozornic")
-                .Address("Studentilor 1/1")
-                .Build();
+            new User().Main();
             
 
-            Delivery delivery = new LinellaDelivery("Линелла");
-            DeliveryMarket market = delivery.Create();
-            Delivery delivery1 = new Nr1Delivery("Номер Один");
-            DeliveryMarket market2 = delivery1.Create();
-
-            //prototype
-            Delivery delivery3 =  delivery.GetClone();
-            delivery3.MarketName = "Фуршет";
-            Console.WriteLine(delivery3.MarketName 
-                               + "\nДоставка из магазина " 
-                               + delivery3.MarketName 
-                               + " для клиента {0} {1} {2}", client1.Name, client1.Surname,client1.Address);
-
-          
-        }
-    }
-
-    abstract class Delivery
-    {
-       
-        public string MarketName { get; set; }
-        
-
-
-        public Delivery(string n)
-        {
-            MarketName = n;
-            
-
-        }
-        public Delivery GetClone()
-        {
-            return (Delivery)this.MemberwiseClone();
-        }
-        // фабричный метод
-        abstract public DeliveryMarket Create();
-    }
-    
-    class LinellaDelivery : Delivery
-    {
-        public LinellaDelivery(string n) : base(n)
-        {
-            Console.WriteLine(this.MarketName);
-        }
-
-        public override DeliveryMarket Create()
-        {
-            return new DeliveryFromLinella();
-        }
-    }
-   
-    class Nr1Delivery : Delivery
-    {
-        public Nr1Delivery(string n) : base(n)
-        {
-            Console.WriteLine( this.MarketName);
-        }
-
-        public override DeliveryMarket Create()
-        {
-            return new DeliveryFromN1();
-        }
-    }
-
-    abstract class DeliveryMarket
-    {
-        
-    }
-
-    class DeliveryFromLinella : DeliveryMarket
-    {
-        public DeliveryFromLinella()
-        {
-            Console.WriteLine("Доставочка из Линеллы клиенту\n");
-        }
-    }
-    class DeliveryFromN1 : DeliveryMarket
-    {
-        public DeliveryFromN1()
-        {
-            Console.WriteLine("Доставочка из магазина Номер один\n");
-        }
-    }
-
-    //builder
-    public class Client
-    {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Address { get; set; }
-    }
-
-    public class ClientBuilder
-    {
-        private readonly Client _client;
-
-        public ClientBuilder()
-        {
-            _client = new Client();
-        }
-
-        public ClientBuilder Name(string name)
-        {
-            _client.Name = name;
-            return this;
-        }
-
-        public ClientBuilder Surname(string surname)
-        {
-            _client.Surname = surname;
-            return this;
-        }
-
-        public ClientBuilder Address(string address)
-        {
-            _client.Address = address;
-            return this;
-        }
-
-
-        public Client Build()
-        {
-
-            return _client;
         }
     }
 }
+

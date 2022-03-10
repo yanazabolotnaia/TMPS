30c86bd67f5e8deef1fb69188dbd85d76837147a
yanazabolotnaia <91680307+yanazabolotnaia@users.noreply.github.com>
2022-03-10 18:50:51 +0200

    Add files via upload

--- Lab1/Abstaract Factory/Program.cs
+++ Lab1/Abstaract Factory/Program.cs
@@ -1,1 +1,115 @@
+﻿using System;
+
+
+namespace AbstractFactory
+{
+    public interface Development
+    {
+        DevelopmentA CreateDesigner();
+        DevelopmentB CreateDeveloper();
+    }
+
+    class AndroidDevelopment : Development
+    {
+        public DevelopmentA CreateDesigner()
+        {
+            return new AndroidDesigner();
+        }
+
+        public DevelopmentB CreateDeveloper()
+        {
+            return new AndroidDeveloper();
+        }
+    }
+
+    class iOSDevelopment : Development
+    {
+        public DevelopmentA CreateDesigner()
+        {
+            return new iOSDesigner();
+        }
+
+        public DevelopmentB CreateDeveloper()
+        {
+            return new iOSDeveloper();
+        }
+    }
+
+    public interface DevelopmentA
+    {
+        string UsefulFunctionA();
+    }
+
+   
+    class AndroidDesigner : DevelopmentA
+    {
+        public string UsefulFunctionA()
+        {
+            return "Дизайнер для андроид приложения найден !";
+        }
+    }
+
+    class iOSDesigner : DevelopmentA
+    {
+        public string UsefulFunctionA()
+        {
+            return "Дизайнер для iOs приложения найден !";
+        }
+    }
+
+    public interface DevelopmentB
+    {
+        string UsefulFunctionB();
+    }
+
+    class AndroidDeveloper : DevelopmentB
+    {
+        public string UsefulFunctionB()
+        {
+            return "Разработчик для андроид приложения найден !";
+        }
+
+    }
+
+    class iOSDeveloper : DevelopmentB
+    {
+        public string UsefulFunctionB()
+        {
+            return "Разработчик для iOS приложения найден !";
+        }
+
+    }
+    class Client
+    {
+        public void Main()
+        {
+            
+            Console.WriteLine("Создаем андроид приложение!!!");
+            ClientMethod(new AndroidDevelopment());
+            Console.WriteLine();
+
+            Console.WriteLine("Создаем iOS приложение!!!");
+            ClientMethod(new iOSDevelopment());
+        }
+
+        public void ClientMethod(Development development)
+        {
+            var appA = development.CreateDesigner();
+            var appB = development.CreateDeveloper();
+
+            Console.WriteLine(appA.UsefulFunctionA());
+            Console.WriteLine(appB.UsefulFunctionB());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+
+            Console.ReadKey();
+        }
+    }
+}
 
